d245c032ecd13619417461d73f2576b7a2f1305b
sdb329 <sdb329@drexel.edu>
2021-11-09 23:51:53 -0500

    set-up

---
+++ dev/js/scripts.js
@@ -0,0 +1,54 @@
+// import { gsap } from "gsap";
+
+
+
+import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { Flip } from "gsap/Flip";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+gsap.registerPlugin(GSDevTools, MorphSVGPlugin, DrawSVGPlugin, Flip, MotionPathHelper, MotionPathPlugin);
+
+// // const 
+// // var
+// // let
+
+
+const mainTL = gsap.timeline();
+
+
+
+function logo() {
+    const tl = gsap.timeline();
+
+    return tl;
+}
+
+
+
+
+
+
+
+mainTL
+    .add(logo())
+
+;
+
+
+
+
+
+
+
+
+
+
+// GSDevTools.create();
+MorphSVGPlugin.create();
+DrawSVGPlugin.create();
+Flip.create();
+MotionPathHelper.create();
+MotionPathPlugin.create();
